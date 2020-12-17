c863c4f3c5d3eff09655f17345c02fd5f6f708ef
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-17 11:39:12 +0200

    move error handling in separate function

--- src/bin/csv.rs
+++ src/bin/csv.rs
@@ -9,21 +9,25 @@ extern crate csv;
 // Import the standard library's I/O module so we can read from stdin.
 use std::io;
 use std::process;
+use std::error::Error;
 
-// The `main` function is where your program starts executing.
 fn main() {
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
     // Create a CSV parser that reads data from stdin.
     let mut rdr = csv::Reader::from_reader(io::stdin());
     // Loop over each record.
     for result in rdr.records() {
-// exam. results ; OK -> Ok() ; problems -> print error message 
+// exam. results ; OK -> Ok() ; problems -> convert error to Box<error> 
         match result {
             Ok(record) => println!("{:?}", record),
-            Err(err) => { 
-                println!("error reading file from <stdin>{}", err);
-            process::exit(1);
-            }
-
+            Err(err) => return Err(From::from(err))
         }
     }
+    Ok(())
 }
