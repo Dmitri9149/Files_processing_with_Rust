aa0d5c26dbb5908cd6d2e44198be33f8cbfca759
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-27 13:15:33 +0200

    allow dead code + allow unused import

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 extern crate csv;
 use std::io;
-use std::process;
+#[allow(unused_imports)] use std::process;
+use std::env;
 use std::error::Error;
+use std::ffi::OsString;
+use std::fs::File;
 
 
 // determened in crate ./bin/csv.rs
 // read from stdin like 
 //$ ./target/debug/csv < uspop.csv
+#[allow(dead_code)]
 pub fn run_stdin() -> Result<(), Box<dyn Error>> {
     // Create a CSV parser that reads data from stdin.
     let mut rdr = csv::Reader::from_reader(io::stdin());
@@ -21,7 +25,8 @@ pub fn run_stdin() -> Result<(), Box<dyn Error>> {
 }
 
 // determened in crate ./bin/csv.rs
-// read from file like 
+// read from file like
+#[allow(dead_code)] 
 fn run_file() -> Result<(), Box<dyn Error>> {
     let file_path = get_first_arg()?;
     let file = File::open(file_path)?;
@@ -31,4 +36,14 @@ fn run_file() -> Result<(), Box<dyn Error>> {
         println!("{:?}", record);
     }
     Ok(())
+}
+
+// Returns the first positional argument sent to this process. If there are no
+// positional arguments, then this returns an error.
+#[allow(dead_code)]
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
 }
