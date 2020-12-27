e3ad2afe83a8ef76d45513703d2337c1b5961607
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-27 10:10:53 +0200

    some small changes

--- src/bin/csv.rs
+++ src/bin/csv.rs
@@ -2,23 +2,19 @@
 // https://docs.rs/csv/1.0.0/csv/tutorial/index.html  
 //////////////////////////////////////////////////////
 
-//tutorial-setup-01.rs
-// This makes the csv crate accessible to your program.
 extern crate csv;
-
-// Import the standard library's I/O module so we can read from stdin.
 use std::io;
 use std::process;
 use std::error::Error;
 
 fn main() {
-    if let Err(err) = run() {
+    if let Err(err) = run_stdin() {
         println!("{}", err);
         process::exit(1);
     }
 }
 
-fn run() -> Result<(), Box<dyn Error>> {
+pub fn run_stdin() -> Result<(), Box<dyn Error>> {
     // Create a CSV parser that reads data from stdin.
     let mut rdr = csv::Reader::from_reader(io::stdin());
     // Loop over each record.
