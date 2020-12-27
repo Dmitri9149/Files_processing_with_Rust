15d210fd1828e8e8cab92262f1a524e9fc9d8178
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-27 13:17:00 +0200

    some additions

--- src/bin/csv.rs
+++ src/bin/csv.rs
@@ -30,6 +30,16 @@ fn run_stdin() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+// Returns the first positional argument sent to this process. If there are no
+// positional arguments, then this returns an error.
+
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
 fn run_file() -> Result<(), Box<dyn Error>> {
     let file_path = get_first_arg()?;
     let file = File::open(file_path)?;
@@ -41,13 +51,4 @@ fn run_file() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-// Returns the first positional argument sent to this process. If there are no
-// positional arguments, then this returns an error.
-
-fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
-    match env::args_os().nth(1) {
-        None => Err(From::from("expected 1 argument, but got none")),
-        Some(file_path) => Ok(file_path),
-    }
-}
 
