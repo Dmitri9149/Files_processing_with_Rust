a98860594f5eb690d26143c6ad6f04185d6626a5
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-27 10:10:32 +0200

    new function is added

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+extern crate csv;
+use std::io;
+use std::process;
+use std::error::Error;
+
+pub fn run_stdin() -> Result<(), Box<dyn Error>> {
+    // Create a CSV parser that reads data from stdin.
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    // Loop over each record.
+    for result in rdr.records() {
+// exam. results ; OK -> Ok() ; problems -> convert error to Box<error> 
+// use ? syntactic sugare
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
