7fe208a6fd8d912cea44a19112b187c5e4bd1b58
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-08 17:04:11 +0200

    bug fixed

--- src/bin/minigrep.rs
+++ src/bin/minigrep.rs
@@ -6,24 +6,24 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file  {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file  {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong with file reading");
 
     println!("The file (text) content:\n{}", contents);
 }
 
 struct Config {
-    query: String;
-    filename: String;
+    query: String,
+    filename: String
 }
 
 fn parse_config(args: &[String]) -> Config {
     let query = args[1].clone();
     let filename = args[2].clone();
-    Config (query, filename)
+    Config { query, filename }
 }
