8c31c6f42b3e91959ee0ec372e0a141f5046d8fc
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-08 16:58:35 +0200

    Config struct is added

--- src/bin/minigrep.rs
+++ src/bin/minigrep.rs
@@ -17,8 +17,13 @@ fn main() {
     println!("The file (text) content:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+struct Config {
+    query: String;
+    filename: String;
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config (query, filename)
 }
