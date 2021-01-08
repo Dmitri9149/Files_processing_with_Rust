c688fe4a0928c4eca383185a5f42571bcc71b09a
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-08 15:29:10 +0200

    read args and print it

--- src/bin/minigrep.rs
+++ src/bin/minigrep.rs
@@ -4,5 +4,11 @@ use std::env;
 // read args from stdin
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file  {}", fiename);
+
 }
